778017028483d3ace7e794dd3c48b5a7143bb6d0
Javiermarist <javiermarist@gmail.com>
2025-04-24 14:01:58 +0200

    Some Updates

--- Assets/Scripts/Game/MainMenuController.cs
+++ Assets/Scripts/Game/MainMenuController.cs
@@ -4,9 +4,12 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     [Header("Scene Names")]
-    public string level1SceneName = "Level1"; // Nombre de la escena del nivel 1
-    public string optionsSceneName = "Options"; // Nombre de la escena de opciones
-    public string guideSceneName = "Guide"; // Nombre de la escena de la guía
+    public string level1SceneName = "Level1";
+    public string level2SceneName = "Level2";
+    public string level3SceneName = "Endless";
+    public string levelSelectorSceneName = "LevelSelector";
+    public string optionsSceneName = "Options";
+    public string guideSceneName = "Guide";
     public string mainMenuSceneName = "MainMenu";
 
     public AudioManager audioManager;
@@ -16,56 +19,69 @@ public class MainMenuController : MonoBehaviour
         audioManager = FindFirstObjectByType<AudioManager>();
     }
 
-    // Método para cargar el nivel 1
-    // Método para cargar el nivel 1 y garantizar que todo se reinicie
-    public void StartGame()
+    public void LoadLevel1()
     {
         PlaySelectSound();
-
-        // Restaurar el tiempo de juego en caso de que esté pausado
+        
         Time.timeScale = 1;
-
-        // Cargar la escena del nivel 1
+        
         SceneManager.LoadScene(level1SceneName);
     }
-
-
-    // Método para cargar la escena de opciones
+    
+    public void LoadLevel2()
+    {
+        PlaySelectSound();
+        
+        Time.timeScale = 1;
+        
+        SceneManager.LoadScene(level2SceneName);
+    }
+    
+    public void LoadEndless()
+    {
+        PlaySelectSound();
+        
+        Time.timeScale = 1;
+        
+        SceneManager.LoadScene(level3SceneName);
+    }
+    
+    public void LevelSelector()
+    {
+        PlaySelectSound();
+        SceneManager.LoadScene(levelSelectorSceneName);
+    }
+    
     public void OpenOptions()
     {
         PlaySelectSound();
         SceneManager.LoadScene(optionsSceneName);
     }
-
-    // Método para cargar la escena de la guía
+    
     public void OpenGuide()
     {
         PlaySelectSound();
         SceneManager.LoadScene(guideSceneName);
     }
-
-    // Método para volver al menú principal
+    
     public void MainMenu()
     {
         PlaySelectSound();
         SceneManager.LoadScene(mainMenuSceneName);
     }
-
-    // Método para salir del juego
+    
     public void QuitGame()
     {
         PlaySelectSound();
         Debug.Log("Saliendo del juego...");
         Application.Quit();
     }
-
-    // Método para reproducir el sonido de selección
+    
     private void PlaySelectSound()
     {
         if (audioManager != null)
         {
             audioManager.menuButtonSoundSource.Play();
-            //AudioManager.Instance.PlaySound(AudioManager.Instance.menuButtonSound);
         }
     }
 }
